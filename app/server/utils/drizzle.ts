ad251977c4cd75603f33c9f79fac5083a4a1bca4
Wellington <wellington.wong@exults.com>
2026-07-22 03:17:44 +0800

    D1 config

---
+++ app/server/utils/drizzle.ts
@@ -0,0 +1,11 @@
+import { drizzle } from 'drizzle-orm/d1'
+import * as schema from '../../database/schema'
+
+
+
+
+export const tables = schema
+
+export function useDrizzle {
+	return drizzle(hubDatabase(), {schema})
+}
