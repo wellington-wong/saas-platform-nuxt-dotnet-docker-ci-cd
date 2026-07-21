18460620f7660a78943170a1d20ecf9907b4a746
Wellington <wellington.wong@exults.com>
2026-07-22 05:31:47 +0800

    Fix compile 500

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
+
+  	vite: {
+  		define: {
+  			'import.meta.url': JSON.stringify('http://localhost')
+  		}
+  	},
   devtools: { enabled: true }
 })
